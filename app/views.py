fb49efb3bca2f98586d1b26fde48780a51ffa632
weili <wei.li@mxplayer.in>
2023-12-08 10:20:05 +0800

    [LW]feat: temp change

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello world !")
+
+
+def user_list(request):
+    return render(request, "user_list.html")
+    # return HttpResponse("User list")
+
+
